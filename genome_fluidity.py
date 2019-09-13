657fd36c111abee477ded524f8da4c21c827d8b9
PlantDr430 <stephenwyka@gmail.com>
2019-09-12 20:26:51 -0600

    created better way to find intial values for powerlaw regression

--- genome_fluidity.py
+++ genome_fluidity.py
@@ -9,7 +9,11 @@ N-1 for each sample of N genomes. Results are a text file of fluidity, variance,
 standard deviation for all N genome samples and a figure of total genome pool fluidity 
 with shaded regions showing standard deviations with a power law regression fit.
 
-If you get OptimizeWarning, you can disregard. It is just a warning and curves were 
+Warnings!
+1. This will only work if you have at least 5 isolates to make up your pangenome.
+2. If you have 5 isolates your graph will probably not look pretty as it's difficult 
+to fit with such a low number of samples. You could try reducing the bootstrapping.
+3. If you get OptimizeWarning, you can disregard. It is just a warning and curves were 
 correctly fit to the data.
 '''
 
@@ -19,6 +23,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+from scipy.optimize import differential_evolution
+import warnings
 
 rundir = os.getcwd()
 
@@ -48,11 +54,11 @@ parser.add_argument(
     metavar=''
 )
 parser.add_argument(
-    '-s',
-    '--simulations',
-    default=50,
+    '-b',
+    '--bootstraps',
+    default=25,
     type=int,
-    help = 'Number of simulations to run [default: 50]',
+    help = 'Number of bootstraps to run [default: 25]',
     metavar=''
 )
 args=parser.parse_args()
@@ -89,8 +95,30 @@ def get_shared_single(pairs):
                 para['Ul'] += 1
     return
 
-def powerlaw(x, m, c, c0):
+def powerlaw(x, c, m, c0):
     return c*(x**m) + c0
+
+def sumOfSquaredError(parameterTuple):
+    warnings.filterwarnings("ignore") # do not print warnings by genetic algorithm
+    val = powerlaw(x_labels, *parameterTuple)
+    return np.sum((err_top - val) ** 2.0)
+
+def generate_Initial_Parameters(level):
+    # min and max used for bounds
+    maxX = max(x_labels)
+    minX = min(x_labels)
+    maxY = max(level)
+    minY = min(level)
+    maxXY = max(maxX, maxY)
+
+    parameterBounds = []
+    parameterBounds.append([-maxXY, maxXY]) # seach bounds for a
+    parameterBounds.append([-maxXY, maxXY]) # seach bounds for b
+    parameterBounds.append([-maxXY, maxXY]) # seach bounds for c
+
+    # "seed" the numpy random number generator for repeatable results
+    result = differential_evolution(sumOfSquaredError, parameterBounds, seed=3)
+    return result.x
 
 # create dictionary of gene clusters and isolates per cluster
 ortho_isolates = OrderedDict()
@@ -125,33 +153,27 @@ for i in range(0, len(iso_list)):
                 all_pair = (para['Uk'] + para['Shared']) + (para['Ul'] + para['Shared'])
                 matrix_dict[pair] = [unique_pair, all_pair]
 
-# print(matrix_dict)
-# sys.exit()
-
-
 boot_dict = {}
 jack_boot_var = {}
 jack_boot_dev = {}
-for b in range(0, args.simulations): # simulations
-    print(b)
+for b in range(0, args.bootstraps): # bootstraps
     genome_sample_dict = {}
     fluidity_dict = {}
     jack_sample_dict = {}
     fluid_i_dict = {}
     jack_variance_dict = {}
     jack_stdev_dict = {}
-    for N in range(4, iso_num + 1): # main run
-        # print(N)
+    for N in range(3, iso_num + 1): # main run
         genome_sample_dict[N] = []
         fluidity_dict[N] = []
         fluid_i_dict[N] = []
         jack_variance_dict[N] = []
         jack_stdev_dict[N] = []
         random_pairs = []
-        
+
         random_genome_sample = random.sample(iso_list, k=N) # random sample of genomes starting with a pool of 4
         get_pairs(random_genome_sample, random_pairs) # loop through random sample and create pairs of genomes
-        
+
         for pair in random_pairs: # for pairs loop through gene cluster dictionary and pull out num of shared / single
             pair = tuple(sorted(pair))
             unique_sum = matrix_dict[pair][0]
@@ -183,29 +205,49 @@ for b in range(0, args.simulations): # simulations
         jack_variance_dict[N].append(fluid_var)
         jack_stdev_dict[N].append(fluid_stdev)
 
-    jack_var = [float(''.join(map(str,v))) for v in jack_variance_dict.values()] # get variance for each N genome pool
+    # add values to jack_boot dictionaries for each boot run
+    jack_variance = [float(''.join(map(str,v))) for v in jack_variance_dict.values()] # get variance for each N genome pool
     jack_stdev = [float(''.join(map(str,v))) for v in jack_stdev_dict.values()] # get stderr for each N genome pool
+    jack_boot_var[b] = jack_variance
+    jack_boot_dev[b] = jack_stdev
+
+# transpose jack_boot dictionaries to lists of means
+jack_var = np.array([v for v in jack_boot_var.values()]).T.tolist()
+final_jack_var = np.array([np.mean(x) for x in jack_var])
+jack_dev = np.array([v for v in jack_boot_dev.values()]).T.tolist()
+final_jack_dev = np.array([np.mean(x) for x in jack_dev])
+
 
 boot_var = []
 boot_stdev = []
-for v in range(4, iso_num + 1):
+for v in range(3, iso_num + 1):
     boot_var.append(np.var(boot_dict[v]))
     boot_stdev.append(np.std(boot_dict[v]))
 
-combined_var = np.array([(boot_var[x] + jack_var[x])/2 for x in range(0, iso_num-3)])
-combined_stdev = np.array([(boot_stdev[x] + jack_stdev[x])/2 for x in range(0, iso_num-3)])
+# get total variance from bootstrap estimates and jackknife estimates
+combined_var = np.array([(boot_var[x] + final_jack_var[x]) for x in range(0, iso_num-2)])
+combined_stdev = np.array([(boot_stdev[x] + final_jack_dev[x]) for x in range(0, iso_num-2)])
 
+# create files paths and get fluidty data and x-axis labels
 fluid_results = os.path.abspath(os.path.join(result_dir, 'Pangenome_fluidity.txt'))
 fluid_fig = os.path.abspath(os.path.join(result_dir, 'Pangenome_fluidity.png'))
 best_fluidity = fluidity_dict[iso_num][0] # get fludity calculated from all genomes in dataset
-overall_data = np.array([best_fluidity for i in range(4, iso_num + 1)]) # create y-value for all x-labels
-x_labels = np.array([i for i in range(4, iso_num + 1)]) # get x-axis label
+overall_data = np.array([best_fluidity for i in range(3, iso_num + 1)]) # create y-value for all x_labels
+x_labels = np.array([i for i in range(3, iso_num + 1)]) # get x-axis label
 
+# get top and bottom values of fludidty +/- stdev
 err_bottom = np.array([(best_fluidity - v) for v in combined_stdev])
 err_top = np.array([(best_fluidity + v) for v in combined_stdev])
 
-popt_t, pcov_t = curve_fit(powerlaw, x_labels, err_top, p0 = np.asarray([0,1,1]), maxfev=2000) # powerlaw fit_top
-popt_b, pcov_b = curve_fit(powerlaw, x_labels, err_bottom, p0 = np.asarray([0,1,1]), maxfev=2000) # powerlaw fit_bottom
+# generate initial parameter values
+geneticParameters_top = generate_Initial_Parameters(err_top)
+geneticParameters_bottom = generate_Initial_Parameters(err_bottom)
+
+# get best fit model parameters. Used large maxfev to make it robust as fitting is 
+# difficult with ~5 genomes, even with generated initial values. Since we are setting 
+# initial values I don't see a concern of having a high maxfev.
+popt_t, pcov = curve_fit(powerlaw, x_labels, err_top, geneticParameters_top, maxfev=10000)
+popt_b, pcov = curve_fit(powerlaw, x_labels, err_bottom, geneticParameters_bottom, maxfev=10000)
 
 fig, ax = plt.subplots()
 ax.set_axisbelow(True)
@@ -219,19 +261,19 @@ plt.plot(x_labels, overall_data, ls='--', lw=1, color='black') # plot y-values o
 plt.fill_between(x_labels, powerlaw(x_labels, *popt_t), powerlaw(x_labels, *popt_b), facecolor='blue', alpha=0.6)
 plt.xticks(np.arange(x_labels[0], x_labels[len(x_labels)-1]+1, 1.0)) # make sure x interval is 1
 plt.xlim(x_labels[0], x_labels[len(x_labels)-1]) # adjust x limit so it starts with 4 at 0
-stdev_top = max(powerlaw(x_labels, *popt_t))
-stdev_bottom = min(powerlaw(x_labels, *popt_b))
-plt.ylim((stdev_bottom - stdev_bottom*0.2), (stdev_top + stdev_top*0.2))
+stdev_top = max(err_top)
+stdev_bottom = min(err_bottom)
+plt.ylim((stdev_bottom - stdev_bottom*0.15), (stdev_top + stdev_top*0.15))
 plt.xlabel('Genomes sampled')
 plt.ylabel('Fluidity, '+u'\u03C6')
 plt.tight_layout()
-plt.show()
-# plt.savefig(fluid_fig)
+plt.savefig(fluid_fig)
 
-# with open(fluid_results, 'w') as results:
-    # results.write('Genomes_Sampled\tFluidity\tVariance\tStderr\n')
-    # r_out = []
-    # for i in range(0, iso_num-3):
-        # r_out.append([str(i+4), str(best_fluidity), str(final_var.tolist()[i]), str(final_err.tolist()[i])])
-    # for line in r_out:
-        # results.write('\t'.join(line) + '\n')
+with open(fluid_results, 'w') as results:
+    results.write('Genomes_Sampled\tFluidity\tVariance\tStderr\tPower_top\tPower_bottom\n')
+    r_out = []
+    for i in range(0, iso_num-2):
+        r_out.append([str(i+4), str(fluidity_dict[i+4][0]), str(combined_var[i]), str(combined_stdev[i]),
+        str(powerlaw(x_labels, *popt_t)[i]), str(powerlaw(x_labels, *popt_b)[i])])
+    for line in r_out:
+        results.write('\t'.join(line) + '\n')
