24d46af8d72f392c306e68e4f082ce18116f7ec2
PlantDr430 <stephenwyka@gmail.com>
2019-09-22 14:51:47 -0600

    reformatted script and added significance table output

--- combine_fluidity.py
+++ combine_fluidity.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
-import os, sys, argparse
+import os, sys, argparse, csv
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from numpy import sqrt, abs
+from scipy.stats import norm
 
 rundir = os.getcwd()
 
@@ -12,11 +14,10 @@ class MyFormatter(argparse.RawTextHelpFormatter):
         super(MyFormatter, self).__init__(prog, max_help_position=48)
 parser = argparse.ArgumentParser(
     usage='./%(prog)s [options] -i XXX_pangenome_fluidity.txt XXX_pangenome_fluidity.txt -o output_folder',
-    description = '''    Turns multiple outputs from genome_fluidity.py into 
-    one figure. Make sure to attach a prefix "XXX_pangenome_fluidity.txt if you 
-    didn't already use the -p / --prefix flag in the previous script, to be 
-    used in the legend''',
-    
+    description = '''    Turns multiple outputs from genome_fluidity.py into a figure and pvalue matrix. 
+    Make sure to attach a prefix "XXX_pangenome_fluidity.txt if you didn't already use the -p / --prefix 
+    flag in the previous script, to be used in the legend''',
+
     epilog = """Written by Stephen A. Wyka (2019)""",
     formatter_class = MyFormatter)
 
@@ -54,46 +55,84 @@ parser.add_argument(
 parser.add_argument(
     '-l',
     '--legend',
-    default = 8,
+    default = 6,
     type=int,
     help = 'Size of legend [default = 8]',
     metavar=''
 )
 args=parser.parse_args()
 
-output_figure = os.path.abspath(os.path.join(rundir, args.output+'.png'))
-
 x_len = []
 data = []
 curve_top = []
 curve_bottom = []
 legend_labels = []
-for file in args.input:
-    legend_labels.append(file.split('_')[0])
-    fpath = os.path.abspath(os.path.join(rundir, file))
+z_test_dict = {} # {Name : [fluidity, genome count, stdev]}
+def parse_input_files(input_file):
+    legend_name = input_file.split('_')[0]
+    legend_labels.append(legend_name)
+    fpath = os.path.abspath(os.path.join(rundir, input_file))
     df=pd.read_csv(fpath, delimiter='\t', header=0)
     x_labels = np.array(df['Genomes_Sampled'])
-    x_len.append(max(x_labels))
-    fluid_best = df['Fluidity'].iloc[len(df['Fluidity'])-1]
-    fluid_data = np.array([fluid_best for x in range(0,len(df['Fluidity']))])
+    genome_count = x_labels[-1]
+    x_len.append(genome_count)
+    stdev = sqrt(df['Total_Variance'].iloc[len(df['Total_Variance'])-1])
+    fluid_all = df['Fluidity'].iloc[len(df['Fluidity'])-1]
+    z_test_dict[legend_name] = [fluid_all,genome_count,stdev]
+    fluid_data = np.array([fluid_all for x in range(0,len(df['Fluidity']))])
     data.append((x_labels, fluid_data))
     curve_top.append(np.array(df['Power_top']))
     curve_bottom.append(np.array(df['Power_bottom']))
 
-fig, ax = plt.subplots(figsize=(args.figsize[0],args.figsize[1]), dpi=args.dpi)
-colors = plt.cm.nipy_spectral([1.*i/len(args.input) for i in range(len(args.input))])
-for i in range(0, len(data)):
-    plt.plot(data[i][0], data[i][1], ls='--', lw=1.5, color=colors[i], alpha=1, label='{}'.format(legend_labels[i]))
-    plt.fill_between(data[i][0], curve_top[i], curve_bottom[i], facecolor=colors[i], alpha=0.25)
-ax.set_facecolor('gainsboro')
-ax.set_axisbelow(True)
-plt.minorticks_on()
-ax.tick_params(axis='x', which='minor', bottom=False)
-ax.grid(which='major', linestyle='-', linewidth='1', color='white')
-plt.xticks(np.arange(3, max(x_len) + 1, 1.0))
-plt.xlim(3, max(x_len))
-plt.xlabel('Genomes sampled')
-plt.ylabel('Fluidity, '+u'\u03C6')
-plt.legend(framealpha=1.0, prop={'size': args.legend})
-plt.tight_layout()
-plt.savefig(output_figure)
+def create_fluidity_figure(output_file):
+    fig, ax = plt.subplots(figsize=(args.figsize[0],args.figsize[1]), dpi=args.dpi)
+    colors = plt.cm.nipy_spectral([1.*i/len(args.input) for i in range(len(args.input))])
+    for i in range(0, len(data)):
+        num_genomes = str(data[i][0][-1])
+        plt.plot(data[i][0], data[i][1], ls='--', lw=1.5, color=colors[i], alpha=1, label='{}'.format(legend_labels[i]+' ('+num_genomes+')'))
+        plt.fill_between(data[i][0], curve_top[i], curve_bottom[i], facecolor=colors[i], alpha=0.25)
+    ax.set_facecolor('lightgrey')
+    ax.set_axisbelow(True)
+    plt.minorticks_on()
+    plt.grid(which='minor', axis='y', color='white', linestyle='--', alpha=0.3)
+    ax.tick_params(axis='x', which='minor', bottom=False)
+    ax.grid(which='major', linestyle='-', linewidth='1', color='white')
+    plt.xticks(np.arange(3, max(x_len) + 1, 1.0))
+    plt.xlim(3, max(x_len))
+    plt.xlabel('Genomes sampled')
+    plt.ylabel('Fluidity, '+u'\u03C6')
+    plt.legend(framealpha=1.0, prop={'size': args.legend})
+    plt.tight_layout()
+    plt.savefig(output_file)
+
+def two_sample_z(X1, n1, sd1, X2, n2, sd2):
+    pooledSE = sqrt(sd1**2/n1 + sd2**2/n2)
+    z = ((X1 - X2) - 0)/pooledSE
+    pval = 2*(norm.sf(abs(z)))
+    return pval
+
+def determine_significance_matrix(output_file):
+    sorted_dict = sorted(z_test_dict.items(), key=lambda x: x[1], reverse=True)
+    result_matrix = []
+    for i in range(0, len(sorted_dict)): # loop through isolates
+        line_lists = ['-'] * (i+1) # add blanks for easy matrix to datafram as we have unequal lengths
+        for m in range(i+1, len(sorted_dict)): # compute pvalues for isolate v. others (no duplicates, no self)
+            args = sorted_dict[i][1] + sorted_dict[m][1] # combine dict values to list for key v. key comparison
+            pval = two_sample_z(args[0],args[1],args[2],args[3],args[4],args[5]) # get pvalue from two-sample z-test
+            short_pval = np.format_float_scientific(pval, precision=2) # shorten pvalue singificant figures
+            line_lists.append(short_pval) # create list of all pvalues for isolate i v. others
+        result_matrix.append([sorted_dict[i][0]] + line_lists) # append all pvalues for isolate i to matrix
+    labels = [sorted_dict[x][0] for x in range(0, len(sorted_dict))] # get headers and index for dataframe
+    with open(output_file, 'w') as outfile:
+        report = csv.writer(outfile, delimiter = '\t', lineterminator='\n')
+        report.writerow([''] + labels)
+        for i in range(0, len(sorted_dict)):
+            report.writerow(result_matrix[i])
+
+if __name__ == "__main__":
+    output_figure = os.path.abspath(os.path.join(rundir, args.output+'.png'))
+    output_pval_table = os.path.abspath(os.path.join(rundir, args.output+'_table.tsv'))
+    for files in args.input:
+        parse_input_files(files)
+    create_fluidity_figure(output_figure)
+    determine_significance_matrix(output_pval_table)
