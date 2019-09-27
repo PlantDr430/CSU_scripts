213e7e77f4bda262de72ed2040315f80811792d4
PlantDr430 <stephenwyka@gmail.com>
2019-09-27 15:26:03 -0600

    Added multithreading, add subsampling max, corrected subsampling

--- genome_fluidity.py
+++ genome_fluidity.py
@@ -3,11 +3,17 @@
 '''
 This script follows formulas put forth in Kislyuk et al. (2011) to calculate genome 
 fluidity of a pangenome dataset. Variance and standard error are estimated as total 
-variance by averaging the variance calculated from bootstrapping samples of N genomes 
-from the total pool of genomes and the variance calculated by the jackknife estimator of 
-N-1 for each sample of N genomes. Results are a text file of fluidity, variance, and 
-standard error for all N genome samples and a figure of total genome pool fluidity 
-with shaded regions showing total standard error with a power law regression fit.
+variance containing both the variance due to subsampling all possible combinations 
+(without replacement) of N genomes from the total pool of genomes and the variance 
+due to the limited number of sampled genomes (variance of the pangenome)(Kislyuk et al. 2011). 
+However, the script has a default max number of subsamples set to 250,000 for each N genomes. 
+This can be altered with the -max_sub / --max_subsamples flag or turned off with the --max_off flag. 
+Turning the max_off will force calculations to be done on all possible subsample combinations 
+of N genomes. For samples of N genomes that were stopped at the max number of subsamples the subsamples 
+are sampled WITH replacement and variance is calculated with a degree of freedom = 1 (i.e. n - 1). 
+Results are a text file of fluidity, variance, and standard error for all N genome samples 
+and a figure of pangenome fluidity with shaded regions showing total standard error with a 
+exponential regression fit.
 
 Notes
 1. This will only work if you have at least 5 isolates to make up your pangenome.
@@ -19,8 +25,9 @@ import os, sys, re, argparse, random, itertools, scipy, warnings
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from itertools import combinations
-from collections import OrderedDict
+from multiprocessing import Pool
+from itertools import combinations, product
+from collections import OrderedDict, Iterable
 from scipy.optimize import curve_fit, differential_evolution
 
 rundir = os.getcwd()
@@ -49,6 +56,28 @@ parser.add_argument(
     required = True,
     help = 'Output folder',
     metavar=''
+)
+parser.add_argument(
+    '-c',
+    '--cpus',
+    type=int,
+    default=1,
+    help = 'Number of cores to use for multiprocessing [default: 1]',
+    metavar=''
+)
+parser.add_argument(
+    '-max_sub',
+    '--max_subsamples',
+    type=int,
+    default=250000,
+    help = 'Max number of subsamples to run on N genomes sampled. [default: 250000]',
+    metavar=''
+)
+parser.add_argument(
+    '--perm_off',
+    action='store_true',
+    help = 'Turn off the max subsamples. This will cause the script sample ALL possible combinations'\
+    'for N genomes',
 )
 parser.add_argument(
     '-p',
@@ -94,13 +123,13 @@ def create_ortho_dictionary(ortho_file): # create dictionary of gene clusters an
             ortho_isolates_dict[cluster] = list(set(iso_list))
     return ortho_isolates_dict
 
-def create_pair_dictionary(ortho_dictionary, isolate_list):
+def create_pair_dictionary(ortho_dictionary):
     print('Creating dictionary of paired ratio values')
     pair_dict = {} # {(Isolate1, Isolate2) : [ratio of sum(unique clusters)/sum(all clusters)]}
-    for i in range(0, len(isolate_list)):
-        for x in range(0, len(isolate_list)):
-            if not isolate_list[i] == isolate_list[x]:
-                pair = tuple(sorted([isolate_list[i], isolate_list[x]]))
+    for i in range(0, len(iso_list)):
+        for x in range(0, len(iso_list)):
+            if not iso_list[i] == iso_list[x]:
+                pair = tuple(sorted([iso_list[i], iso_list[x]]))
                 if not pair in pair_dict.keys():
                     cogs = {'Shared' : 0, 'Uk' : 0, 'Ul' : 0}
                     for k,v in ortho_dictionary.items():
@@ -117,36 +146,71 @@ def create_pair_dictionary(ortho_dictionary, isolate_list):
                     pair_dict[pair] = unique_pair/all_pair
     return pair_dict
 
-def compute_fluidity_all_genomes(pair_dictionary, iso_num):
-    fluidity_list = [ratio for ratio in pair_dictionary.values()]
-    pangenome_fluidity = (2/(iso_num*(iso_num-1)))*sum(fluidity_list)
-    return pangenome_fluidity
+def compute_fluidity_all_genomes():
+    N = iso_num
+    fluidity_list = [ratio for ratio in pair_dict.values()]
+    pangenome_fluidity = (2/(N*(N-1)))*sum(fluidity_list)
+    jack_samples = list(combinations(iso_list, N - 1))
+    fluidity_i_list = []
+    for sample in jack_samples:
+        jack_pairs = tuple(combinations(sample,2))
+        jack_sample_fluidity = [pair_dict[tuple(sorted(p))] for p in jack_pairs]
+        fluidity_i = (2/((N-1)*(N-2)))*sum(jack_sample_fluidity)
+        fluidity_i_list.append(fluidity_i)
+    fluidity_i_mean = np.mean(fluidity_i_list)
+    fluidity_variance = ((N-1)/N)*sum([(i-fluidity_i_mean)**2 for i in fluidity_i_list])
+    return pangenome_fluidity, fluidity_variance
 
-def jackknife_genome_subsamples(pair_dictionary, iso_num, iso_list):
-    jack_var_dict = {} # {N genomes sampled : jackknife variance}
+def subsample_multiprocess(combo_list):
+    N = len(combo_list[0])
+    sample_process_list = []
+    for sample in combo_list:
+        pairs = tuple(combinations(sample,2))
+        pair_fluidity_list = [pair_dict[tuple(sorted(p))] for p in pairs]
+        sample_fluidity = (2/(N*(N-1)))*sum(pair_fluidity_list)
+        sample_process_list.append(sample_fluidity)
+    return sample_process_list
+
+def genome_subsamples_fluidities(perm_list):
+    sub_fluid_dict = {} # {N genomes sampled : [list of fluidities from subsamples]}
     for N in range(3, iso_num + 1):
-        print('Performing jackknife permutations of {} genomes sampled'.format(N))
-        jack_var_dict[N] = []
+        sub_fluid_dict[N] = []
         N_combos = list(combinations(iso_list, N))
-        for sample in N_combos:
-            jack_sample = list(combinations(sample, N-1))
-            fluidity_i_list = []
-            for j_sample in jack_sample:
-                pairs = tuple(combinations(j_sample,2))
-                j_sample_fluidity = [pair_dictionary[tuple(sorted(p))] for p in pairs]
-                fluidity_i = (2/((N-1)*(N-2)))*sum(j_sample_fluidity)
-                fluidity_i_list.append(fluidity_i)
-            fluidity_i_mean = np.mean(fluidity_i_list)
-            fluidity_variance = ((N-1)/N)*sum([(i-fluidity_i_mean)**2 for i in fluidity_i_list])
-            jack_var_dict[N].append(fluidity_variance)
-    return jack_var_dict
+        if args.max_off:
+            combos = N_combos
+        else:
+            if len(N_combos) > args.max_subsamples:
+                combos = random.choices(N_combos, k=args.max_subsamples)
+                perm_list.append(N)
+            else:
+                combos = N_combos
+        print('Performing fluidity calculations on {} subsample combinations of {} genomes'.format(len(combos),N))
+        if not len(N_combos) == 1:
+            chunk = round(len(combos)/args.cpus)
+            split_combos = [combos[i:i + chunk] for i in range(0, len(combos), chunk)]
+            pool = Pool(processes=args.cpus)
+            results = pool.imap(subsample_multiprocess, split_combos)
+            sub_fluid_dict[N].append(results)
+        else:
+            last_run = subsample_multiprocess(N_combos)
+            sub_fluid_dict[N].append(last_run)
+        sub_fluid_dict[N]=list(flatten(sub_fluid_dict[N]))
+    return sub_fluid_dict
 
-def powerlaw(x, a, b, c):
-    return a*(x**b) + c
+def flatten(lis):
+     for item in lis:
+         if isinstance(item, Iterable) and not isinstance(item, str):
+             for x in flatten(item):
+                 yield x
+         else:
+             yield item
+
+def exponential(x, a, b, c):
+    return a * np.exp(-b * x) + c
 
 def sumOfSquaredError(parameterTuple, x_values, y_curve_values):
     warnings.filterwarnings("ignore") # do not print warnings by genetic algorithm
-    val = powerlaw(x_values, *parameterTuple)
+    val = exponential(x_values, *parameterTuple)
     return np.sum((y_curve_values - val) ** 2.0)
 
 def generate_Initial_Parameters(x_values, y_curve_values,):
@@ -161,22 +225,27 @@ def generate_Initial_Parameters(x_values, y_curve_values,):
     parameterBounds.append([-maxXY, maxXY]) # seach bounds for a
     parameterBounds.append([-maxXY, maxXY]) # seach bounds for b
     parameterBounds.append([-maxXY, maxXY]) # seach bounds for c
-
     # "seed" the numpy random number generator for repeatable results
     result = differential_evolution(sumOfSquaredError, parameterBounds, args=(x_values,y_curve_values), seed=3)
     return result.x
 
-def create_fluidity_results(variance_dictionary, pangenome_fluidity, iso_num, figure_output, results_output):
-    total_variance = np.array([np.mean(variance_dictionary[i]) for i in range(3, isolate_num + 1)])
+def create_fluidity_results(figure_output, results_output):
+    total_variance = []
+    for i in range(3, iso_num + 1):
+        if i in permutation_list:
+            total_variance.append(np.var(sub_fluid_dict[i], ddof = 1) + pan_varaince)
+        else:
+            total_variance.append(np.var(sub_fluid_dict[i]) + pan_varaince)
+    total_variance = np.array(total_variance)
     total_stderr = np.array([x**(1/2) for x in total_variance])
-    y_fluidity_values = np.array([pangenome_fluidity for i in range(3, isolate_num + 1)])
-    x_labels = np.array([i for i in range(3, isolate_num + 1)])
-    stderr_bottom = np.array([(pangenome_fluidity - v) for v in total_stderr])
-    stderr_top = np.array([(pangenome_fluidity + v) for v in total_stderr])
+    y_fluidity_values = np.array([pan_fluidity for i in range(3, iso_num + 1)])
+    x_labels = np.array([i for i in range(3, iso_num + 1)])
+    stderr_bottom = np.array([(pan_fluidity - v) for v in total_stderr])
+    stderr_top = np.array([(pan_fluidity + v) for v in total_stderr])
     geneticParameters_top = generate_Initial_Parameters(x_labels, stderr_top)
     geneticParameters_bottom = generate_Initial_Parameters(x_labels, stderr_bottom)
-    popt_t, pcov = curve_fit(powerlaw, x_labels, stderr_top, geneticParameters_top, maxfev=10000)
-    popt_b, pcov = curve_fit(powerlaw, x_labels, stderr_bottom, geneticParameters_bottom, maxfev=10000)
+    popt_t, pcov = curve_fit(exponential, x_labels, stderr_top, geneticParameters_top, maxfev=10000)
+    popt_b, pcov = curve_fit(exponential, x_labels, stderr_bottom, geneticParameters_bottom, maxfev=10000)
     fig, ax = plt.subplots()
     ax.set_axisbelow(True)
     plt.minorticks_on()
@@ -186,31 +255,35 @@ def create_fluidity_results(variance_dictionary, pangenome_fluidity, iso_num, fi
     ax.tick_params(axis='x', which='minor', bottom=False)
     ax.set_facecolor('gainsboro')
     plt.plot(x_labels, y_fluidity_values, ls='--', lw=1, color='black') # plot y-values of fluidity
-    plt.fill_between(x_labels, powerlaw(x_labels, *popt_t), powerlaw(x_labels, *popt_b), facecolor='blue', alpha=0.6)
+    plt.fill_between(x_labels, exponential(x_labels, *popt_t), exponential(x_labels, *popt_b), facecolor='blue', alpha=0.6)
     plt.xticks(np.arange(x_labels[0], x_labels[len(x_labels)-1]+1, 1.0)) # make sure x interval is 1
-    plt.xlim(x_labels[0], x_labels[len(x_labels)-1]) # adjust x limit so it starts with 4 at 0
+    plt.xlim(x_labels[0], x_labels[len(x_labels)-1]) # adjust x limit so it starts with 3 at 0
     max_y = max(stderr_top)
     min_y = min(stderr_bottom)
     plt.ylim((min_y - min_y*0.15), (max_y + max_y*0.15))
     plt.xlabel('Number of genomes sampled')
     plt.ylabel('Fluidity, '+u'\u03C6')
     plt.tight_layout()
+    # plt.show()
     plt.savefig(figure_output)
 
     with open(results_output, 'w') as results: # print out fluidity results
-        results.write('Genomes_Sampled\tFluidity\tTotal_Variance\tTotal_Stderr\tPower_top\tPower_bottom\n')
+        results.write('Genomes_Sampled\tFluidity\tTotal_Variance\tTotal_Stderr\tExponential_top\tExponential_bottom\n')
         r_out = []
         for i in range(0, iso_num-2):
-            r_out.append([str(i+3), str(pangenome_fluidity), str(total_variance[i]), str(total_stderr[i]), 
-            str(powerlaw(x_labels, *popt_t)[i]), str(powerlaw(x_labels, *popt_b)[i])])
+            r_out.append([str(i+3), str(pan_fluidity), str(total_variance[i]), str(total_stderr[i]), 
+            str(exponential(x_labels, *popt_t)[i]), str(exponential(x_labels, *popt_b)[i])])
         for line in r_out:
             results.write('\t'.join(line) + '\n')
 
 if __name__ == "__main__":
     ortho_dict = create_ortho_dictionary(input_file)
-    isolate_num = max([len(v) for v in ortho_dict.values()])
-    isolate_list = list(set(itertools.chain.from_iterable([v for v in ortho_dict.values() if len(v) == isolate_num])))
-    pair_dict = create_pair_dictionary(ortho_dict, isolate_list)
-    pan_fluidity = compute_fluidity_all_genomes(pair_dict, isolate_num)
-    jack_varaince_dict = jackknife_genome_subsamples(pair_dict, isolate_num, isolate_list)
-    create_fluidity_results(jack_varaince_dict, pan_fluidity, isolate_num, fluid_fig, fluid_results)
+    iso_num = max([len(v) for v in ortho_dict.values()])
+    iso_list = list(set(itertools.chain.from_iterable([v for v in ortho_dict.values() if len(v) == iso_num])))
+    pair_dict = create_pair_dictionary(ortho_dict)
+    pan_results = compute_fluidity_all_genomes()
+    pan_fluidity = pan_results[0]
+    pan_varaince = pan_results[1]
+    permutation_list = []
+    sub_fluid_dict = genome_subsamples_fluidities(permutation_list)
+    create_fluidity_results(fluid_fig, fluid_results)
